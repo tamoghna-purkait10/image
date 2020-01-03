d732fc725c57e3204504d0c327a4e60eaada8e38
tamoghna-purkait10 <tamoghna10@hotmail.com>
2020-01-03 14:43:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-3xjard
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-3xjard)
---
+++ src/app/image-upload/image-upload.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-upload',
+  templateUrl: './image-upload.component.html',
+  styleUrls: ['./image-upload.component.css']
+})
+export class ImageUploadComponent implements OnInit {
+
+  url: string;
+  onSelectFile(event) { // called each time file input changes
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]); // read file as data url
+
+      reader.onload = (event) => { // called once readAsDataURL is completed
+        this.url = event.target.result;
+      }
+    }
+}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
